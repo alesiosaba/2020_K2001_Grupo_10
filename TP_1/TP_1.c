5e61f03fc36a670db7f7d8b2dd313d61eee2e4bf
alesiosaba <alesiosaba@gmail.com>
2020-04-23 20:12:27 -0300

    borro comentarios e implemento algunas funciones

--- TP_1/TP_1.c
+++ TP_1/TP_1.c
@@ -7,6 +7,7 @@ const int columnas=6;
 void MostrarTabla(int [][columnas]);
 void ProcesarArchivoEntrada(FILE*, FILE*, int [][columnas]);
 void EscribirTipoConstante(FILE*, int);
+void AsignarEstado(int,int*,int [][columnas],int);
 
 int main()
 {
@@ -18,7 +19,8 @@ int main()
 								  {5,5,6,6,6,6},
 								  {6,6,6,6,6,6}};
 	
-	printf("Ver tabla de transiciones?\n1.Si\n0.No\nOpcion: ");
+	
+	printf("Ver tabla de transiciones?\n1.Si\n0.No\n\nOpcion: ");
 	int verTabla = scanf("%d", &verTabla);
 	
 	if(verTabla)
@@ -35,100 +37,49 @@ int main()
     return 0;
 }
 
-void ProcesarArchivoEntrada(FILE *entrada, FILE *salida, int tablaTransiciones[][columnas])
-{
-	int estado;
-	int auxEstado;
+void ProcesarArchivoEntrada(FILE *entrada, FILE *salida, int tablaTransiciones[][columnas]){
+	int estadoActual;
+	int estadoAnterior;
 	
 	char caracter;
 	caracter = fgetc(entrada);
     
-	while(caracter != EOF)
-	{
-		estado = 0;
-		auxEstado = 0;
+	while(caracter != EOF){
+		estadoActual = 0;
+		estadoAnterior = 0;
 		
-		while(caracter != ',' && estado != 6 && caracter != EOF)
+		while(caracter != ',' && estadoActual != 6 && caracter != EOF)
 		{
-			//printf("%c",caracter);
-   	    
-			// Si viene un '0'
+			// Caracter leido es '0'
         	if (caracter == '0')
-			{
-				//printf("Viene un '0'\n");
-				//printf("Estado parado: q%d\n",estado);
-	        	auxEstado = estado;
-	            estado = tablaTransiciones[auxEstado][0];
-	            //printf("Voy a estado q%d\n",estado);
-	            //system("PAUSE");
-	            //system("CLS");
-			}
-        	// Si viene un [1-7] 
+        		AsignarEstado(estadoAnterior,&estadoActual,tablaTransiciones,0);
+        	// Caracter leido es un [1-7] 
 	        else if(caracter > '0' && caracter < '8')
-	        {
-	            //printf("Viene un [1-7]\n");
-				//printf("Estado parado: q%d\n",estado);
-	        	auxEstado = estado;
-	            estado = tablaTransiciones[auxEstado][1];
-	            //printf("Voy a estado q%d\n",estado);
-	            //system("PAUSE");
-	            //system("CLS");
-	        }
-	        // Si viene un {8,9}
+	        	AsignarEstado(estadoAnterior,&estadoActual,tablaTransiciones,1);
+	        // Caracter leido es un {8,9}
 	        else if(caracter == '8' || caracter == '9')
-	        {
-	            //printf("Viene un {8,9}\n");
-				//printf("Estado parado: q%d\n",estado);
-	        	auxEstado = estado;
-	            estado = tablaTransiciones[auxEstado][2];
-	            //printf("Voy a estado q%d\n",estado);
-	            //system("PAUSE");
-	            //system("CLS");
-	        }
-	        // Si viene un {xX}
+	        	AsignarEstado(estadoAnterior,&estadoActual,tablaTransiciones,2);
+	        // Caracter leido es un {xX}
 	        else if(caracter == 'x' || caracter == 'X')
-	        {
-	            //printf("Viene un {xX}\n");
-				//printf("Estado parado: q%d\n",estado);
-	        	auxEstado = estado;
-	            estado = tablaTransiciones[auxEstado][3];
-	            //printf("Voy a estado q%d\n",estado);
-	            //system("PAUSE");
-	            //system("CLS");
-	        }
-	        // Si viene [a-f A-F] 
-	        else if(((caracter >= 'A' && caracter <= 'F') || (caracter >= 'a' && caracter < 'f'))) 
-	        {
-	            //printf("Viene un [a-f A-F]\n");
-				//printf("Estado parado: q%d\n",estado);
-	        	auxEstado = estado;
-	            estado = tablaTransiciones[auxEstado][4];
-	            //printf("Voy a estado q%d\n",estado);
-	            //system("PAUSE");
-	            //system("CLS");
-	        }
-	        // Si viene un caracter no admitido
+	        	AsignarEstado(estadoAnterior,&estadoActual,tablaTransiciones,3);
+	        // Caracter leido es un [a-f A-F] 
+	        else if(((caracter >= 'A' && caracter <= 'F') || (caracter >= 'a' && caracter < 'f')))
+	        	AsignarEstado(estadoAnterior,&estadoActual,tablaTransiciones,4);
+	        // Caracter leido no es admitido
 	        else
-			{
-				//printf("Viene un caracter no admitido\n");
-				//printf("Estado parado: q%d\n",estado);
-	        	auxEstado = estado;
-	            estado = tablaTransiciones[auxEstado][5];
-	            //printf("Voy a estado q%d\n",estado);
-	            //system("PAUSE");
-	            //system("CLS");
-			}
+	        	AsignarEstado(estadoAnterior,&estadoActual,tablaTransiciones,5);
 			
 			fflush(stdin);
 			caracter = fgetc(entrada);
 		}
 		
-		EscribirTipoConstante(salida,estado);
+		EscribirTipoConstante(salida,estadoActual);
 		
 		fflush(stdin);
 		caracter = fgetc(entrada);	
 		
-		if(caracter != EOF && caracter != ',' ) //Para que no agregue coma al final del archivo
+		// Agregar coma despues de cada tipo de constante
+		if(caracter != EOF && caracter != ',' ) 
 			fputs(",",salida);
 	}
 	
@@ -136,11 +87,9 @@ void ProcesarArchivoEntrada(FILE *entrada, FILE *salida, int tablaTransiciones[]
 	fclose(salida);
 }
 
-void MostrarTabla(int tablaTransiciones[][columnas])
-{
-	// Mostrar Tabla de Transicion en pantalla
+void MostrarTabla(int tablaTransiciones[][columnas]){
 	int i,j;
-	printf("\nTabla de Transiciones: \n");
+	printf("\nTabla de Transiciones: \n\n");
     for(i=0; i<filas ; i++)
     {
         printf("q%d  ",i);
@@ -156,37 +105,31 @@ void MostrarTabla(int tablaTransiciones[][columnas])
     system("CLS");
 }
 
-void EscribirTipoConstante(FILE *salida, int estado)
-{
-	// Palabra no reconocida
-		if(estado == 6)
-		{
-			//printf("\nPalabra no reconocida\n");
+void EscribirTipoConstante(FILE *salida, int estadoActual){
+	switch (estadoActual){
+		// Palabra no reconocida
+		case 6:
 			fputs("ERROR",salida);
-			//system("PAUSE");
-		}
-		else 
-		{
-			// Constante Decimal 
-			if(estado == 1)
-			{
-				//printf("\nDecimal\n");
-				fputs("Decimal",salida);
-				//system("PAUSE");
-			}
-			// Constante Octal
-			if(estado == 5 || estado == 2)
-			{
-				//printf("\nOctal\n");
-				fputs("Octal",salida);
-				//system("PAUSE");
-			}
-			// Constante Hexadecimal
-			if(estado == 4)
-			{
-				//printf("\nHexadecimal\n");
-				fputs("Hexadecimal",salida);
-				//system("PAUSE");
-			}	
-		}
+			break;
+		// Constante Decimal 
+		case 1:
+			fputs("Decimal",salida);
+			break;
+		// Constante Octal
+		case 5:
+		case 2:
+			fputs("Octal",salida);
+			break;
+		// Constante Hexadecimal
+		case 4:
+			fputs("Hexadecimal",salida);
+			break;			
+	}
 }
+
+void AsignarEstado(int estadoAnterior, int *estadoActual, int tablaTransiciones[][columnas],int nuevoEstado){
+	estadoAnterior = *estadoActual;
+	*estadoActual = tablaTransiciones[estadoAnterior][nuevoEstado];
+}
+
+

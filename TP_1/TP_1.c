a3f94f5fbee2960a429da4339cbde8612f4d71a8
lsabalagos <lsabalagos@gmail.com>
2020-04-23 16:23:57 -0300

    Reduzco main para mejor lectura

--- TP_1/TP_1.c
+++ TP_1/TP_1.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <windows.h>
 
+const int filas=7;
+const int columnas=6; 
+
+void MostrarTabla(int [][columnas]);
+void ProcesarArchivoEntrada(FILE*, FILE*, int [][columnas]);
+void EscribirTipoConstante(FILE*, int);
+
 int main()
 {
-    int filas=7;
-	int columnas=6; 
     int tablaTransiciones [7][6]={{2,1,1,6,6,6},
 								  {1,1,1,6,6,6},
 								  {5,5,6,3,6,6},
@@ -13,35 +18,29 @@ int main()
 								  {5,5,6,6,6,6},
 								  {6,6,6,6,6,6}};
 	
+	printf("Ver tabla de transiciones?\n1.Si\n0.No\nOpcion: ");
+	int verTabla = scanf("%d", &verTabla);
 	
-	// Mostrar Tabla de Transicion en pantalla
-	/*
-	int i,j;
-	printf("Tabla de Transiciones: \n");
-    for(i=0; i<filas ; i++)
-    {
-        printf("q%d  ",i);
-        for(j=0;j<columnas;j++)
-        {
-            printf("q%d ",tablaTransiciones[i][j]);
-        }
-        printf("\n");
-    }
-    
-    printf("\n");
-    system("PAUSE");
-    system("CLS");
-    */
-	       
-    char caracter;
-    
-    int estado;
-	int auxEstado;
-    
+	if(verTabla)
+		MostrarTabla(tablaTransiciones);
+		
+    FILE * entrada = fopen("entrada.txt","rt");
 	FILE * salida = fopen("salida.txt","wt");
 	
-	FILE * entrada = fopen("entrada.txt","rt");
+	ProcesarArchivoEntrada(entrada,salida,tablaTransiciones);
 	
+	printf("Archivo de entrada procesado exitosamente\n");
+	printf("Archivo de salida grabado exitosamente\n");
+	
+    return 0;
+}
+
+void ProcesarArchivoEntrada(FILE *entrada, FILE *salida, int tablaTransiciones[][columnas])
+{
+	int estado;
+	int auxEstado;
+	
+	char caracter;
 	caracter = fgetc(entrada);
     
 	while(caracter != EOF)
@@ -123,12 +122,47 @@ int main()
 			fflush(stdin);
 			caracter = fgetc(entrada);
 		}
+		
+		EscribirTipoConstante(salida,estado);
+		
+		fflush(stdin);
+		caracter = fgetc(entrada);	
+		
+		if(caracter != EOF && caracter != ',' ) //Para que no agregue coma al final del archivo
+			fputs(",",salida);
+	}
+	
+	fclose(entrada); 
+	fclose(salida);
+}
 
-		// Palabra no reconocida
+void MostrarTabla(int tablaTransiciones[][columnas])
+{
+	// Mostrar Tabla de Transicion en pantalla
+	int i,j;
+	printf("\nTabla de Transiciones: \n");
+    for(i=0; i<filas ; i++)
+    {
+        printf("q%d  ",i);
+        for(j=0;j<columnas;j++)
+        {
+            printf("q%d ",tablaTransiciones[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\n");
+    system("PAUSE");
+    system("CLS");
+}
+
+void EscribirTipoConstante(FILE *salida, int estado)
+{
+	// Palabra no reconocida
 		if(estado == 6)
 		{
 			//printf("\nPalabra no reconocida\n");
-			fputs("error,",salida);
+			fputs("ERROR",salida);
 			//system("PAUSE");
 		}
 		else 
@@ -137,31 +171,22 @@ int main()
 			if(estado == 1)
 			{
 				//printf("\nDecimal\n");
-				fputs("Decimal,",salida);
+				fputs("Decimal",salida);
 				//system("PAUSE");
 			}
 			// Constante Octal
 			if(estado == 5 || estado == 2)
 			{
 				//printf("\nOctal\n");
-				fputs("Octal,",salida);
+				fputs("Octal",salida);
 				//system("PAUSE");
 			}
 			// Constante Hexadecimal
 			if(estado == 4)
 			{
 				//printf("\nHexadecimal\n");
-				fputs("Hexadecimal,",salida);
+				fputs("Hexadecimal",salida);
 				//system("PAUSE");
 			}	
 		}
-		fflush(stdin);
-		caracter = fgetc(entrada);	
-	}
-	
-	fclose(entrada); 
-	fclose(salida);
-		
-    return 0;
 }
-
